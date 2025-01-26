1bcfbf0e28c2bb2c8d50f1c46927a4c431932ef0
vtcong2299 <vtcong2299@gmail.com>
2025-01-26 11:17:34 +0700

    add class Manager

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [SerializeField] AudioSource audioBGM;
+    [SerializeField] AudioSource audioSFX;
+    [SerializeField] AudioClip clickButtonClip;
+    [SerializeField] AudioClip bgmClip;
+    [SerializeField] AudioClip getDameClip;
+    [SerializeField] AudioClip gameOverClip;
+    [SerializeField] AudioClip levelUpClip;
+
+    private void Start()
+    {
+        SoundBGM();
+    }
+    public void SetVolumAudioBGM(bool state)
+    {
+        if (state)
+        {
+            audioBGM.volume = 0.5f;
+        }
+        else
+        {
+            audioBGM.volume = 0;
+        }
+    }
+    public void SetVolumAudioSFX(bool state)
+    {
+        if (state)
+        {
+            audioSFX.volume = 1f;
+        }
+        else
+        {
+            audioSFX.volume = 0;
+        }
+    }
+    public void SoundClickButton()
+    {
+        audioSFX.clip = clickButtonClip;
+        audioSFX.Play();
+    }
+    public void SoundBGM()
+    {
+        audioBGM.clip = bgmClip;
+        audioBGM.loop = true;
+        audioBGM.Play();
+    }
+    public void SoundGetDame()
+    {
+        audioSFX.clip = getDameClip;
+        audioSFX.Play();
+    }
+    public void SoundGameOver()
+    {
+        audioSFX.clip = gameOverClip;
+        audioSFX.Play();
+    }
+    public void SoundLevelUp()
+    {
+        audioSFX.clip = levelUpClip;
+        audioSFX.Play();
+    }
+}
---
+++ Assets/_Scripts/DataGamePlay.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using UnityEngine.TextCore.Text;
+using System.ComponentModel;
+
+[Serializable]
+public class GameData
+{
+    public double highestWave;
+    public bool hasBGM;
+    public bool hasSFX;
+    public float damageDefault = 50;
+    public float hpDefault = 500;
+    public float attackCountdownDefault = 4;
+    public float moveSpeedDefault = 3.5f;
+    //public float armorDefault = 20;
+    public float lifeStealPercentDefault = 0;
+
+}
+
+public class DataGamePlay : Singleton<DataGamePlay>, IGameData, IDataManipulator
+{
+    private string dataPath;
+
+    GameData gameData;
+
+    public GameData data => gameData;
+
+    public void StartDataGamePlay()
+    {
+        dataPath = Path.Combine(Application.persistentDataPath, "playerData.json");
+        gameData = LoadData(); //load khi khởi động
+    }
+
+    public void SaveData()
+    {
+        string json = JsonUtility.ToJson(gameData); //chuyển data thành chuỗi json
+            File.WriteAllText(dataPath, json); //tạo file nếu chưa tồn tại
+    }
+
+    GameData LoadData()
+    {
+        if (File.Exists(dataPath))
+        {
+                string json = File.ReadAllText(dataPath);
+                GameData data = JsonUtility.FromJson<GameData>(json);
+                return data;
+        }
+        return new GameData(); // Trả về dữ liệu mới nếu không tìm thấy file
+    }    
+}
